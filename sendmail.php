ef0059041af9c3e6cefa756521c1a6dcd3912cd4
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-08-18 18:57:05 +0300

    test 2 | work temp

--- sendmail.php
+++ sendmail.php
@@ -8,8 +8,8 @@
 
   if(isset($_POST["send"])){
     $mail = new PHPMailer(true);
-    $mail->Charset = 'UTF-8';
-
+    $mail->CharSet = 'UTF-8';
+    
     $mail->IsSMTP();
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
@@ -19,17 +19,34 @@
     $mail->Port = 465;
 
     $mail->From = 'cinemakyiv@gmail.com';
+    $mail->FromName = $_POST['firstName'].' '.$_POST['lastName'];
     $mail->addAddress('vladyslavketov@gmail.com');
 
     $mail->IsHTML(true);
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $work = $_POST["work"];
-    $message = $_POST["message"];
+    // $subject = $_POST["subject"];
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["message"];
+    $tel = $_POST["tel"];
+    $subject = 'Анкета'.''.$_POST['firstName'].' '.$_POST['lastName'];
 
-    $body = "<h1>Зустрічай новий лист</h1>" . $name . ' ' . $email . ' ' . $work . ' ' . $message;
-    $mail->Subject = 'Заявка з форми 1';
+    $mail->Subject = $subject;
+    
+    $body = '<strong>Актор:</strong> '.$_POST['firstName'].' '.$_POST['lastName'].'';
+      if(trim(!empty($_POST['firstName']))){
+      $body.='<p><strong>Ім`я:</strong> '.$_POST['firstName'].'</p>';
+      }
+      if(trim(!empty($_POST['lastName']))){
+        $body.='<p><strong>Прізвище:</strong> '.$_POST['lastName'].'</p>';
+      }
+      if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Повідомлення:</strong> '.$_POST['message'].'</p>';
+      }
+      if(trim(!empty($_POST['tel']))){
+        $body.='<p><strong>Телефон</strong> '.$_POST['tel'].'</p>';
+      }
+
     $mail->Body = $body;
 
     $mail->send();
